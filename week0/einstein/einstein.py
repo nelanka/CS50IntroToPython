8398bde5f3a1e882a9d1cbd61e304553ca40d001
Nelanka Perera <nelanka.perera@icloud.com>
2024-12-01 07:26:13 -0800

    Added more human understandable measures of energy

--- week0/einstein/einstein.py
+++ week0/einstein/einstein.py
@@ -2,7 +2,18 @@
 # Einstein
 # https://cs50.harvard.edu/python/2022/psets/0/einstein/
 
+# How much energy is in the mass given
 c = 300000000
 mass = float(input("Enter mass in kg: "))
-print("Energy is", mass * c * c, "Joules")
+energy = mass * c * c
+print(f"Energy is {energy} or {int(energy):,} Joules")
 
+# How much water can this boil
+joulesToBoilALiterOfWater = 334400
+litersBoiled = energy/joulesToBoilALiterOfWater
+print(f"This is enough energy to boil {int(litersBoiled):,} liters of water")
+
+# How much water is that actually
+litersPerOlympicPool = 2500000
+numPools = litersBoiled/litersPerOlympicPool
+print(f"This is the equivalent of {int(numPools):,} Olympic sized swimming pools")
